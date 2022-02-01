e8c8e60f1d3c59aa2d837e92990a8ebb8d28c7c1
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-02-01 13:55:41 +0530

    Update variantsIni.ts

--- client/variantsIni.ts
+++ client/variantsIni.ts
@@ -1,4 +1,8 @@
 export const variantsIni = `
 # Hybrid of antichess and atomic.
 [antiatomic:giveaway]
+blastOnCapture = true
+
+# Hybrid variant of antichess and king of the hill
+[atomickoth:kingofthehill]
 blastOnCapture = true`
